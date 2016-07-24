acd28398626a509c3c997a907b9e3892c2634bd8
Kushal Jayswal <kutec@users.noreply.github.com>
2016-07-24 17:19:39 +0530

    Created by Coding Ground Client

---
+++ 01 First JS on Server/main.js
@@ -0,0 +1,10 @@
+var aboutMe = {
+    fullName: "Kushal Jayswal",
+    expertise: "UI/UX and Frontend Technologies",
+    currentExperience: "About 4 years with CD",
+    myBlog: "https://teckstack.com",
+    personalSite: "https://kjayz.com",
+    githubTwitter: "@kutec"
+}
+
+console.log(aboutMe);
