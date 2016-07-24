0765c3322270e931295f7852be6fb7c4b62d0560
Kushal Jayswal <kutec@users.noreply.github.com>
2016-07-24 20:23:37 +0530

    Created by Coding Ground Client

---
+++ 02 Module and Require 1/main.js
@@ -0,0 +1,18 @@
+/*************************************************************
+    1. greetings.js (annonymous function)
+    2. greetingsDir (annonymous function with variable scope)
+    3. OS Core Module
+************************************************************/
+
+//-------- 1. greetings.js
+var greet = require('./greetings');
+greet("Good evening");
+
+//-------- 2. greetingsDir
+// var greetigsFromDir = require('./greetingsDir');
+// globalVar = "Hi - ::: ";
+// greetigsFromDir("Abc");
+
+//-------- 3. Current OS
+// var os = require('os');
+// console.log("Current OS is " +os.type());
